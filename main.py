001555d1ebbe342aad8f6647184a94125ac1ca36
A7exG0 <sasha.gorgun@gmail.com>
2024-06-21 13:20:28 +0300

    Divide game display on top-part for record and bottom-part for game

--- main.py
+++ main.py
@@ -5,15 +5,27 @@ pygame.init()
 
 MAX_X = 600
 MAX_Y = 600
+RECORD_HEIGHT = MAX_Y // 15
+
 # Здесь делаем окно игры 
 display = pygame.display.set_mode((MAX_X, MAX_Y))
 pygame.display.set_caption('Snake Game')
 
-
+# Зададим параметры цветов
 snake_color = blue = (0,0,255)  # rgb(0,0,255)
 background_color = white = (255, 255, 255) #rgb(255, 255, 255)
 snake_lose_color = (255, 0, 0) # rgb(255, 0, 0)
 black = (0, 0, 0)
+
+# Создадим верхню панель для фиксирования результата 
+record_surface = pygame.Surface((MAX_X, RECORD_HEIGHT))
+record_surface.fill(blue)
+display.blit(record_surface, (0, 0))
+
+# Создадим нижнюю игровую панель
+game_surface = pygame.Surface((MAX_X, MAX_Y - RECORD_HEIGHT))
+game_surface.fill(white)
+display.blit(game_surface, (0, RECORD_HEIGHT))
 
 # Задаем начальные координаты змейки 
 x1 = 300
@@ -30,10 +42,12 @@ def display_game_over():
     text_surface = pygame.Surface((300, 100))
     text_surface.fill(blue)  # Заполнение поверхности черным цветом
 
+    # Наносим на на новый Surface текст
     font = pygame.font.SysFont(None, 24)
     text = font.render('Game Over! Press R to Restart', True, white)
     text_surface.blit(text, (30, 20))
 
+    # Выводим созданную менюшку на главный экран 
     display.blit(text_surface, (MAX_X // 4, MAX_Y // 4))
     pygame.display.update()
 
@@ -59,22 +73,14 @@ while(running):
 
     new_x1 = x1 + x1_change 
     new_y1 = y1 + y1_change 
-
+    # print(MAX_Y - RECORD_HEIGHT, new_x1, new_y1)
     if new_x1 < 0 or MAX_X <= new_x1: 
-        clock.tick(0)
         pygame.draw.rect(display, snake_lose_color, [x1, y1, 20, 20])
-        # pygame.display.update()
         game_over = True
 
-    if new_y1 < 0 or MAX_Y <= new_y1: 
-        clock.tick(0)
+    if new_y1 < RECORD_HEIGHT or MAX_Y <= new_y1: 
         pygame.draw.rect(display, snake_lose_color, [x1, y1, 20, 20])
-        # pygame.display.update()
         game_over = True
-
-    x1 = new_x1 
-    y1 = new_y1 
-    clock.tick(5)
 
     if game_over:
         display_game_over()
@@ -88,8 +94,14 @@ while(running):
                         game_over = False 
                         x1 = 300 
                         y1 = 300    
+                        # Очищаем игровую область 
+                        display.blit(game_surface, (0, RECORD_HEIGHT))
+                        pygame.display.update()
     else:
-        display.fill(background_color) # заливаем все белым
+        pygame.draw.rect(display, white, [x1, y1, 20, 20]) # заливаем область, откуда ушла змейка
+        x1 = new_x1 
+        y1 = new_y1 
+        clock.tick(5)
         pygame.draw.rect(display, snake_color, [x1, y1, 20, 20]) # x1, y1 - новая позиция змейки; 20, 20 - размеры кубика
         pygame.display.update()
 
